0d07c9e74753a8089a6d320337fd4a822fa0d768
erieneodeleon <erineodeleon5@gmail.com>
2020-09-08 13:53:52 -0600

    finalizado

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+
+        public string ceo ;
+
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(2000,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEcsula = new Escuela();
+            miEcsula.nombre = "Platzi Academy";
+            miEcsula.direccion = "Cr 9 calle 72";
+            miEcsula.añoFundacion =  2012 ;
+            Console.WriteLine("Timbre");
+            miEcsula.Timbrar();
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
